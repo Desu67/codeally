dcfb2c7e48417e89e4f934c2a1d5b98e85400264
Desu67 <crkg123@gmail.com>
2021-12-21 01:46:55 +0100

    notification click system and routes user logged

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
+// pages
 import Apps from './apps'
+import Error from '../web/pages/error'
 // axios
 import Axios from 'axios'
 // firebase
 import firebase from '../../utils/firebase'
-// navigate
-import { useNavigate } from 'react-router'
+// routes
+import { Routes, Route, useNavigate } from 'react-router-dom'
+// images
+import icon_codeally from '../../assets/icon.png'
 
 function Codeally(props) {
 
@@ -34,14 +38,41 @@ function Codeally(props) {
         }
     })
 
+    const showNoti = () => {
+        const options = {
+            body: "Tokyo send friend request",
+            icon: icon_codeally,
+            dir: "ltr",
+            link: "https://codeally.herokuapp.com/"
+        }
+        const noti = new Notification('Codeally', options)
+
+        noti.onclick = () => {
+            navigate('/games')
+        }
+    }
+
+    return (
+        <Routes>
+            <Route path="/" element={<User apps={apps} user={user} logout={logout} showNoti={showNoti} />}/>
+            <Route path="/games" element={<h1>lol</h1>}/>
+            <Route path="*" element={<Error navigate={navigate}/>}/>
+        </Routes>
+    )
+}
+
+export default Codeally
+
+function User (props) {
+
+    const { apps, user, logout, showNoti } = props
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', width: '100%', minHeight: '100vh' }}>
             <Apps apps={apps}/>
             <h1>{user.displayName}</h1>
             <button onClick={() => logout()}>logout</button>
-            <button onClick={() => new Notification('Hey')}>Notification</button>
+            <button onClick={() => showNoti()}>Notification</button>
         </div>
     )
 }
-
-export default Codeally
