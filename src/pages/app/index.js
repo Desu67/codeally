92b0d5ad4c806a4efe47e66162b1fba47c8610da
Desu67 <crkg123@gmail.com>
2021-12-21 01:03:09 +0100

    notification system

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -26,11 +26,20 @@ function Codeally(props) {
         })
     }, [apps])
 
+    useEffect(() => {
+        if (!("Notification" in window)) {
+            console.log("This browser does not support desktop notification")
+        } else {
+            Notification.requestPermission()
+        }
+    })
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', width: '100%', minHeight: '100vh' }}>
             <Apps apps={apps}/>
             <h1>{user.displayName}</h1>
             <button onClick={() => logout()}>logout</button>
+            <button onClick={() => new Notification('Hey')}>Notification</button>
         </div>
     )
 }
