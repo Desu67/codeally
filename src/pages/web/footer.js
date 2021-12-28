83c83fd9b97be3bf0712d12f75c490b359e4b512
Desu67 <crkg123@gmail.com>
2021-12-28 12:03:46 +0100

    footer links

--- src/pages/web/footer.js
+++ src/pages/web/footer.js
@@ -21,12 +21,36 @@ function Footer() {
         }
     })
 
+    const openurl = (url) => {
+        window.open(
+            url,
+            '_blank'
+        )
+    }
+
     return (
         <div className="footer" style={{ background: Colors.primary }}>
             <div className='row'>
-                <img src={twitter} alt="twitter"/>
-                <img src={discord} alt="discord"/>
-                <img src={youtube} alt="instagram"/>
+                <img 
+                    onClick={() => 
+                        openurl('https://twitter.com/CodeallyTeam')} 
+                    src={twitter}
+                    alt="twitter"
+                />
+
+                <img 
+                    onClick={() => 
+                        openurl('https://discord.gg/2tMks4TYwC')} 
+                    src={discord} 
+                    alt="discord"
+                />
+
+                <img 
+                    onClick={() => 
+                        openurl('https://www.youtube.com/channel/UCG18gvH-bIDDMP73_6mGmvA')} 
+                    src={youtube} 
+                    alt="instagram"
+                />
             </div>
             <h1 style={{ fontSize: fontSize }}>Copyright © {year.getFullYear()} Codeally. All Rights Reserved.</h1>
         </div>
